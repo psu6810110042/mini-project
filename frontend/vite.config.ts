0ed3e2c12276913f9e96709e2bd41da5842605af
Jirakorn Sukmee <6810110042@psu.ac.th>
2025-12-31 16:37:27 +0700

    added cloudflare tunnel for sharing

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,19 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
+    allowedHosts: true,
     host: true, // เพิ่มบรรทัดนี้เพื่อให้ Docker เข้าถึงได้ง่ายขึ้น
     proxy: {
-      '/api': {
+      "/api": {
         // ❌ ของเดิม: target: 'http://localhost:3000',
         // ✅ ของใหม่: เปลี่ยน localhost เป็นชื่อ service ใน docker-compose
-        target: 'http://backend:3000', 
+        target: "http://backend:3000",
         changeOrigin: true,
         secure: false,
       },
     },
   },
-})
+});
